e6e93cb7d0999861771e09df93a6c4ff48e8f2e0
Ashleigh Maund <ashleighmaund@gmail.com>
2015-06-18 10:05:21 +0100

    added feature and unit tests - displays average rating for all reviews and nothing when there are no reviews

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 feature 'reviewing' do
   before {Restaurant.create name: 'KFC'}
 
+  def leave_review(thoughts, rating)
+    visit '/'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
   scenario 'allow users to leave a review using a form' do 
     visit '/restaurants'
     click_link 'Review KFC'
@@ -12,5 +20,11 @@ feature 'reviewing' do
 
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('lovely', 3)
+    leave_review('not lovely', 5)
+    expect(page).to have_content('Average rating: 4')
   end
 end
